29fc909062b8bd0971b6f3d1103db47d6081acfe
gywns022727 <lifford050227a@gmail.com>
2023-08-26 18:02:27 +0900

    create /com/test/domain/Book.java

--- src/test/java/com/test/demo/DemoApplicationTests.java
+++ src/test/java/com/test/demo/DemoApplicationTests.java
@@ -1,13 +1,32 @@
 package com.test.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(properties = {"value=test", "property.value=propertyTest"},
+		classes = DemoApplication.class,
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+	@Value("${value}")
+	private String value;
+
+	@Value("${property.value}")
+	private String propertyValue;
 
 	@Test
 	void contextLoads() {
+		assertThat(value, is("test"));
+		assertThat(propertyValue, is("propertyTest"));
 	}
 
 }
+
+
+
+
+
